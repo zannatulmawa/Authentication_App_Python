0d70c46faac17b896744268935d05b30a3a91dee
Zannat <zannatulmawa62@gmail.com>
2022-06-13 16:46:37 -0400

    updated

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 #Blueprint means it has many routes and urls in it.
 #we can add multiple files of views inside this
@@ -10,5 +10,5 @@ views = Blueprint('views', __name__)
 #@view notation will go to the requested route or url
 @views.route('/')
 def home():
-    return "<h1>Python App</h1>"
+    return render_template("home.html")
 
